1f9f8cf52f6a63a66946e34c7c02796bcdce7fe5
JHANSIRANI41 <127426371+JHANSIRANI41@users.noreply.github.com>
2024-01-17 20:45:16 +0530

    Add files via upload

---
+++ datatypes2.js
@@ -0,0 +1,16 @@
+/*
+
+Primitive datatypes in js
+1.number
+2.BigInt
+3.String =>" "
+4.null =>standalone value
+5.undefined =>no value is assigned
+6.symbol =>unique values
+7.Boolean =>true/false
+
+ */
+
+//object
+console.log(typeof null); //output :object
+console.log(typeof undefined); //output:undefined
