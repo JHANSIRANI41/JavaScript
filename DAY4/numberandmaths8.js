eecb7cfb6ff5cfd8031d299dc449fb427e5db215
JHANSIRANI41 <127426371+JHANSIRANI41@users.noreply.github.com>
2024-01-24 22:36:48 +0530

    Add files via upload

---
+++ DAY4/numberandmaths8.js
@@ -0,0 +1,44 @@
+/////////////////////////NUMBERS///////////////////////
+// const ans = 456
+// console.log(ans);
+
+// const value = new Number(56454.546);
+// console.log(value);
+// //convert to string
+// console.log(value.toString().length);
+// console.log(value.toFixed(2));
+// console.log(value.toPrecision(5));
+// const zeros = 1000000;
+// console.log(zeros.toLocaleString('en-IN'));
+
+
+//////////////////////////MATHS//////////////////////////
+// console.log(Math.abs(-1));
+// console.log(Math.PI);
+// console.log(Math.min(2, 5, 1, 4));
+// console.log(Math.max(2, 5, 1, 4));
+// console.log(Math.round(2.3));
+// console.log(Math.floor(3.4));
+// console.log(Math.ceil(4.5));
+//math.random (values always lies between (0-1))
+// console.log(Math.random());
+// console.log(Math.floor(((Math.random()) * 10) + 1)); //(1-9)
+
+// const min = 10
+// const max = 20
+
+
+// //important formula
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+// let dicemin = 1;
+// let dicemax = 6;
+// console.log(Math.floor(Math.random() * (dicemax - dicemin + 1) + dicemin));
+
+
+//console.log(Math.max());//if there are no parameters then it gives -infinity
+//power function
+// console.log(Math.pow(2, 3));
+// //truncate function
+// console.log(Math.trunc(34.234));
+console.log(Math.MAX_VALUE);
+console.log(Math.MIN_VALUE);
