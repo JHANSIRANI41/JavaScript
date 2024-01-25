e01d0beb3e59ebc0d9a3bf26fd90f0e14de3b78d
JHANSIRANI41 <127426371+JHANSIRANI41@users.noreply.github.com>
2024-01-25 22:46:59 +0530

    Add files via upload

---
+++ DAY6/objectbasics.js
@@ -0,0 +1,11 @@
+//object creation
+const obj1 = {
+    name: "ram",
+    age: 98,
+    id: 8976,
+    "full name": "ramraj"
+
+}
+console.log(obj1.name);
+console.log(obj1["full name"]);
+console.log(obj1.age);
